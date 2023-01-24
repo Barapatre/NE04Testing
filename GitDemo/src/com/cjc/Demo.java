6225b30d463ba9614bf62c93bebbd583b1f7693d
Siddhesh <sbarapatre7494@gmail.com>
2023-01-25 00:13:07 +0530

    By Ujwala

--- GitDemo/src/com/cjc/Demo.java
+++ GitDemo/src/com/cjc/Demo.java
@@ -6,6 +6,8 @@ public class Demo {
 		
 	System.out.println("Hello java");
 	System.out.println("Git Demo");
+	
+
 	}
 
 }
---
+++ GitDemo/src/com/cjc/Demo1.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+		System.out.println("Welcome");
+
+	}
+
+}
