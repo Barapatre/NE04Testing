feecc49c0af11f9bf9045348e4f4ab7555506d7a
Barapatre <123442166+Barapatre@users.noreply.github.com>
2023-01-25 00:19:34 +0530

    By Git

--- GitDemo/src/com/cjc/Demo.java
+++ GitDemo/src/com/cjc/Demo.java
@@ -4,6 +4,12 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
+	public void m1()
+	{
+	System.out.println("CJC Classes");
+	}
+		
+		
 	System.out.println("Hello java");
 	System.out.println("Git Demo");
 	System.out.println("Selenium");
